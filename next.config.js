be9319bc6511e04d11a67c844fbb6b3b59940c93
Andre Birkus <andre.birkus@deep6.ai>
2023-10-18 09:36:27 -0500

    changing font

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-// };
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+};
+
 module.exports = nextConfig;
